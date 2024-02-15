164bd0342b3e410c803383ab8bb184cfbe824dbc
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-02-15 02:47:49 +0000

    Bump pkg version

--- src/kfp_component_lib/__init__.py
+++ src/kfp_component_lib/__init__.py
@@ -1,4 +1,4 @@
 """The kfp_component_lib package."""
-VERSION = "0.1.0.dev0"
+VERSION = "0.1.0"
 PKG_DEPENDENCY = [f"kfp_component_lib=={VERSION}", "--find-links=dist"]
 KFP_CONTAINER_IMAGE = f"alexioannides/kfp-component-lib:{VERSION}"
